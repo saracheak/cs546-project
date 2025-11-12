18601e30890aeda4767a5e235b7f06e016c66b2c
saracheak <saracheak@gmail.com>
2025-11-12 01:16:49 -0500

    Added biscuit definitions

--- tasks/seed.js
+++ tasks/seed.js
@@ -27,6 +27,29 @@ let parksData = [
     }
 ]
 
+let biscuitsData = [
+    {
+        biscuit_name : "First Park Visit",
+        description : "User visited their first park!"
+    },
+    {
+        biscuit_name : "First Comment",
+        description : "User left their first comment on a park!"
+    },
+    {
+        biscuit_name : "You've got a friend in me",
+        description : "User liked another pupfile!"
+    },
+    {
+        biscuit_name : "Park Runner",
+        description : "User visited three parks!"
+    },
+    {
+        biscuit_name : "Popularity",
+        description : "User's comment received 10 likes"
+    },
+]
+
 const main = async () => {
     const parksCollection = await parks();
     // await parksCollection.deleteMany({});
