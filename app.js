86d6835e02cd3171bbed2a1a230e2ec190d6f283
saracheak <saracheak@gmail.com>
2025-11-20 01:38:59 -0500

    Added basic login and profile page

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import exphbs from 'express-handlebars'
 const app = express();
 import configRoutes from './routes/index.js';
 
+app.use(express.static('/public'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 configRoutes(app);
 
