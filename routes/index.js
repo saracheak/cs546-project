a03a1482cd4fdcf51e6a810dfa54eb7229f2b656
saracheak <saracheak@gmail.com>
2025-12-02 00:47:29 -0500

    fixed routes merge issue

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,19 @@ import loginRoutes from './login.js'
 import signupRoutes from './signup.js'
 import ratingRoutes from './ratings.js'
 import editPupfileRoutes from "./editPupfile.js";
+import homeRoutes from "./home.js";
+import biscuitsRoutes from "./biscuits.js";
 
 const constructorMethod = (app) => {
+  app.use('/parks', parksRoutes);
   app.use('/profile', profileRoutes);
   app.use('/login', loginRoutes);
   app.use('/signup', signupRoutes);
   app.use('/parks', ratingRoutes); 
-  app.use("/editPupfile", editPupfileRoutes);
-
-//   app.use('/users', userRoutes);
-//   app.use('/biscuits', biscuitsRoutes);
-
-  app.use("/parks", parksRoutes);
-
+  app.use('/editPupfile', editPupfileRoutes);
+  app.use('/home', homeRoutes);
+  app.use('/biscuits', biscuitsRoutes);
+  
   app.use(/(.*)/, (req, res) => {
     res.status(404).json({ error: 'Not found' });
   });
